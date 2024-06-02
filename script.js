72c0ef53622be0c25095dd31e47d5d055893e9df
Mubashir Ali <21sw071@students.muet.edu.pk>
2024-06-03 00:58:01 +0500

    Compeleted Chrome Extension

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 const saveInput = document.querySelector("#input-btn");
-let leads = [];
+let currentleads = [];
 const inputField = document.querySelector("#input-field");
 const leadsList = document.querySelector("#leads-list");
+const deleteButton = document.querySelector("#delete-btn");
+const locaLeads = JSON.parse(localStorage.getItem("leads"));
+const saveTabButton = document.querySelector("#savetab-btn");
+
+
+if(locaLeads){
+    currentleads = locaLeads;
+    showLeads(currentleads); 
+}
+
 saveInput.addEventListener("click", function() {
-    leads.push(inputField.value);
+    currentleads.push(inputField.value);
     inputField.value = "";
-    showLeads(); 
+    localStorage.setItem("leads", JSON.stringify(currentleads));
+    showLeads(currentleads); 
 })
 
-function showLeads(){
+deleteButton.addEventListener("click", function(){      
+    currentleads = [];
+    localStorage.clear();
+    showLeads(currentleads);
+})
+
+saveTabButton.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        currentleads.push(tabs[0].url);
+        localStorage.setItem("leads",JSON.stringify(currentleads));
+        showLeads(currentleads);
+    })
+})
+
+function showLeads(leads){
     let listItems = "";
     for(let i = 0; i < leads.length; i++){
         listItems += `
